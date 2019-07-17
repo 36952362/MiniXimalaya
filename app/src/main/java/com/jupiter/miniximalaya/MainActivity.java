4a0c815888bae57301d396a3e27533859ba8b8a1
Nigel Tang (haijutan) <haijutan@cisco.com>
2019-07-17 19:58:17 +0800

    integrate with Ximalaya SDK and verify

--- app/src/main/java/com/jupiter/miniximalaya/MainActivity.java
+++ app/src/main/java/com/jupiter/miniximalaya/MainActivity.java
@@ -3,12 +3,40 @@ package com.jupiter.miniximalaya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, String> map = new HashMap<String, String>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList object) {
+                List<Category> categories = object.getCategories();
+                for(Category category : categories){
+                    Log.i(TAG, category.getCategoryName());
+                }
+            }
+
+            @Override
+            public void onError(int code, String message) {
+                Log.e(TAG, "Get Categories failed, code:" + code + ", message: " + message);
+            }
+        });
     }
 }
